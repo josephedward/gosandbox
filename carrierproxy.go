ae98c22ffe1143eb818f2b6ccf73ce0ae8a0334a
josephedward <josephedwardwork@gmail.com>
2022-07-30 14:33:53 -0400

    first commit
    - want to use a scraper to login to acloud guru for me
    - need to implement dom traversal after login

--- carrierproxy.go
+++ carrierproxy.go
@@ -2,10 +2,62 @@ package main
 
 import (
 	"io"
+	// "github.com/chromedp/chromedp"
+	// "context"
+	// "errors"
+	"fmt"
+	"github.com/joho/godotenv"
+
+	// "math/rand"
+	// "net/http"
+	// "path/filepath"
+	// "sync"
+	"time"
+	// "github.com/chromedp/chromedp"
+	"log"
+	"os"
+	"github.com/go-rod/rod"
+	// "github.com/go-rod/rod/lib/cdp"
+	"github.com/go-rod/rod/lib/input"
+	// "github.com/go-rod/rod/lib/launcher"
+	// "github.com/go-rod/rod/lib/proto"
+	// "github.com/go-rod/rod/lib/utils"
+	// "github.com/ysmood/gson"
 )
 
 type PolicyProvider interface {
 	Login(username, password string) error
 	Policies() ([]Policy, error)
 	DocumentDownload(downloadKey string) (io.ReadCloser, error)
+}
+
+func main() {
+	// Launch a new browser with default options, and connect to it.
+	browser := rod.New().MustConnect()
+
+	// Even you forget to close, rod will close it after main process ends.
+	defer browser.MustClose()
+
+	// Create a new page
+	page := browser.MustPage("https://learn.acloud.guru/cloud-playground/cloud-sandboxes")
+
+	// fmt.Println(page)
+	log.Print(page)
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Some error occured. Err: %s", err)
+	}
+
+	username := os.Getenv("USERNAME")
+	password := os.Getenv("PASSWORD")
+
+	fmt.Println(username)
+	fmt.Println(password)
+
+	
+	page.MustElement("input[name='email']").MustInput(username).MustType(input.Enter)
+	page.MustElement("input[name='password']").MustInput(password).MustType(input.Enter)
+	page.MustWaitLoad().MustScreenshot("a.png")
+
 }
---
+++ go.mod
@@ -0,0 +1,12 @@
+module goscraper
+
+go 1.18
+
+require github.com/go-rod/rod v0.108.1
+
+require (
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/ysmood/goob v0.4.0 // indirect
+	github.com/ysmood/gson v0.7.2 // indirect
+	github.com/ysmood/leakless v0.8.0 // indirect
+)
