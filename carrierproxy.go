0f0ebb543e67b097873e76654d1e049c9fcb8ed5
josephedward <josephedwardwork@gmail.com>
2022-07-31 22:55:36 -0400

    full poc working for cmd line tool
    still need to implement the interface for assignment

--- carrierproxy.go
+++ carrierproxy.go
@@ -2,28 +2,14 @@ package main
 
 import (
 	"io"
-	
-	// "github.com/chromedp/chromedp"
-	// "context"
-	// "errors"
 	"fmt"
 	"github.com/joho/godotenv"
-	// "math/rand"
-	// "net/http"
-	// "path/filepath"
-	// "sync"
 	"time"
-	// "github.com/chromedp/chromedp"
+	"github.com/go-rod/rod"
 	"log"
 	"os"
-	"github.com/go-rod/rod"
-	// "github.com/go-rod/rod/lib/cdp"
 	"github.com/go-rod/rod/lib/input"
-	// "github.com/go-rod/rod/lib/launcher"
-	// "github.com/go-rod/rod/lib/proto"
-	// "github.com/go-rod/rod/lib/utils"
-	// "github.com/ysmood/gson"
-	// "github.com/otiai10/gosseract/v2"
+	"golang.design/x/clipboard"
 )
 
 type PolicyProvider interface {
@@ -52,22 +38,70 @@ func main() {
 
 	//login to the page
 	username := os.Getenv("USERNAME")
-	password := os.Getenv("PASSWORD")	
+	password := os.Getenv("PASSWORD")
 	page.MustElement("input[name='email']").MustInput(username).MustType(input.Enter)
 	page.MustElement("input[name='password']").MustInput(password).MustType(input.Enter)
 
 	//start a sandbox
-	page.MustElementR("button","Start AWS Sandbox").MustClick()
+	page.MustElementR("button", "Start AWS Sandbox").MustClick()
 	page.MustWaitLoad().MustScreenshot("creds.png")
-		
-	//MustElements immediately returns empty list if no element found, so we wait for it. 
+
+	//MustElements immediately returns empty list if no element found, so we wait for it.
 	time.Sleep(6 * time.Second)
 
 	// div[attr^="elem"]
-	vals :=	page.MustElements("div[class^='CopyableInstanceField__Value']")
-	
-	for _, v := range vals {
-		fmt.Println(v.MustText())
+	vals := page.MustElements("div[class^='CopyableInstanceField__Value']")
+
+	//have to copy to clipboard to get whole string
+	vals[3].MustElement("svg[aria-label='copy icon']").MustClick()
+
+	//initialize cliboard package
+	clipboard.Init()
+	if err != nil {
+		panic(err)
 	}
-		
+
+	//new entry for aws sandbox creds
+	appendLine("\n\n")
+	appendLine("[cloud_user]")
+
+	// write/read text format data of the clipboard, and
+	// the byte buffer regarding the text are UTF8 encoded.
+	keyid := clipboard.Read(clipboard.FmtText)
+	
+	//zero out the clipboard just in case 
+	clipboard.Write(clipboard.FmtText, nil)
+	
+	fmt.Println(keyid)
+	//append key id to the file
+	appendLine("aws_access_key_id = " + string(keyid))
+
+	vals[4].MustElement("svg[aria-label='copy icon']").MustClick()
+	accessKey := clipboard.Read(clipboard.FmtText)
+	clipboard.Write(clipboard.FmtText, nil)
+	fmt.Println(accessKey)
+
+	//append access key to the file
+	appendLine("aws_secret_access_key = " + string(accessKey))
+}
+
+func appendLine(newLine string) {
+	f, err := os.OpenFile("../../../.aws/credentials", os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	_, err = fmt.Fprintln(f, newLine)
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("file appended successfully")
 }
--- go.mod
+++ go.mod
@@ -2,14 +2,18 @@ module goscraper
 
 go 1.18
 
-require github.com/go-rod/rod v0.108.1
+require (
+	github.com/go-rod/rod v0.108.1
+	github.com/joho/godotenv v1.4.0
+	golang.design/x/clipboard v0.6.2
+)
 
 require (
-	github.com/deluan/lookup v0.0.7 // indirect
-	github.com/joho/godotenv v1.4.0 // indirect
-	github.com/otiai10/gosseract/v2 v2.4.0 // indirect
 	github.com/ysmood/goob v0.4.0 // indirect
 	github.com/ysmood/gson v0.7.2 // indirect
 	github.com/ysmood/leakless v0.8.0 // indirect
-	gocv.io/x/gocv v0.31.0 // indirect
+	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
+	golang.org/x/mobile v0.0.0-20210716004757-34ab1303b554 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 )
