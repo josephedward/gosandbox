641adaf3edfa3b9e5a8aace7a2c6bf6603ff9892
josephedward <josephedwardwork@gmail.com>
2022-10-11 12:12:40 -0400

    WIP - remote control for rod container
    - will remove additional 'main' methods, there for testing now
    - need to refactor everything to use container and remote

--- core/browser.go
+++ core/browser.go
@@ -1,20 +1,20 @@
 package core
 
 import (
-	"os"
-	"os/exec"
+	"flag"
+	"fmt"
 	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/launcher"
 	"github.com/go-rod/rod/lib/utils"
 	"github.com/ysmood/leakless"
-	"flag"
-	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/exec"
 )
 
-func CustomLaunch() *rod.Browser{
+func CustomLaunch() *rod.Browser {
 	// get the browser executable path
 	path := launcher.NewBrowser().MustGet()
 
@@ -36,7 +36,6 @@ func CustomLaunch() *rod.Browser{
 	return rod.New().ControlURL(u).MustConnect()
 }
 
-
 func UseSystemBrowser() *rod.Browser {
 	if path, exists := launcher.LookPath(); exists {
 		u := launcher.New().Bin(path).MustLaunch()
@@ -45,21 +44,20 @@ func UseSystemBrowser() *rod.Browser {
 	return nil
 }
 
-
-func BrowserCliOutput() *rod.Browser{
+func BrowserCliOutput() *rod.Browser {
 	// Pipe the browser stderr and stdout to os.Stdout .
 	u := launcher.New().Logger(os.Stdout).MustLaunch()
 	return rod.New().ControlURL(u).MustConnect()
 }
 
-func Manual(u string) *rod.Browser{
+func Manual(u string) *rod.Browser {
 	return rod.New().ControlURL(u).MustConnect()
 }
-
 
 var addr = flag.String("address", "localhost:7317", "the address to listen to")
 var quiet = flag.Bool("quiet", false, "silence the log")
 var allowAllPath = flag.Bool("allow-all", false, "allow all path set by the client")
+
 // A server to help launch browser remotely
 func manager() {
 	flag.Parse()
@@ -85,4 +83,33 @@ func manager() {
 
 	srv := &http.Server{Handler: m}
 	utils.E(srv.Serve(l))
+}
+
+func remote() {
+	// This example is to launch a browser remotely, not connect to a running browser remotely,
+	// to connect to a running browser check the "../connect-browser" example.
+	// Rod provides a docker image for beginers, run the below to start a launcher.Manager:
+	//
+	//     docker run -p 7317:7317 ghcr.io/go-rod/rod
+	//
+	// For more information, check the doc of launcher.Manager
+	l := launcher.MustNewManaged("")
+
+	// You can also set any flag remotely before you launch the remote browser.
+	// Available flags: https://peter.sh/experiments/chromium-command-line-switches
+	l.Set("disable-gpu").Delete("disable-gpu")
+
+	// Launch with headful mode
+	// l.Headless(false).XVFB("--server-num=5", "--server-args=-screen 0 1600x900x16")
+
+	browser := rod.New().Client(l.MustClient()).MustConnect()
+
+	// You may want to start a server to watch the screenshots of the remote browser.
+	launcher.Open(browser.ServeMonitor(""))
+
+	fmt.Println(
+		browser.MustPage("https://mdn.dev/").MustEval("() => document.title"),
+	)
+
+	utils.Pause()
 }
